49358f43a3af41911fa4afce58d21963c4eaf252
Sai Manoj <saimanojanangi6@gmail.com>
2026-03-10 15:23:37 +0530

    update dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  reactCompiler: false,
+  experimental: {
+    reactCompiler: false,
+  },
 };
 
 export default nextConfig;
